338a7d6ac6e3f7a68dfc5749732d683b9da61951
numbafye <saadiqgp@gmail.com>
2023-11-04 22:47:07 -0400

    python functions all added, eat lunch works

--- functions_practice.py
+++ functions_practice.py
@@ -8,5 +8,14 @@ def pack(arg1, arg2, arg3):
 
 print(pack(1, 2, 3))
 
-def eat_lunch():
-     
+def eat_lunch(list):
+    if len(list) == 0:
+        print("My lunchbox is empty")
+    else:
+        for i in range(len(list)):
+            if i == 0:
+                print(f"First I eat {list[0]}")
+            else:
+                print(f"Next I eat {list[i]}")
+eat_lunch(["cake", "apple", "cookie"])
+eat_lunch([])
